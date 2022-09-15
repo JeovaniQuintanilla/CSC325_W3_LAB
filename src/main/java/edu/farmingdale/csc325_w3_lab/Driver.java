0c58c20d2f8bf86821d8e2cb1b808052f7a9a379
quint <quint@137.125.136.30>
2022-09-15 09:43:48 -0400

    Init projects

---
+++ src/main/java/edu/farmingdale/csc325_w3_lab/Driver.java
@@ -0,0 +1,13 @@
+
+package edu.farmingdale.csc325_w3_lab;
+
+
+public class Driver {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("HI");
+        
+    }
+    
+}
